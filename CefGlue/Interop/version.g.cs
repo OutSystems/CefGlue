e280ca13a7a7c37eaca0af10369c6bc6b4ff8dc3
Dmitry Azaraev <dmitry.azaraev@gmail.com>
2012-06-23 12:29:09 +0300

    Updated to R705.

--- CefGlue/Interop/version.g.cs
+++ CefGlue/Interop/version.g.cs
@@ -9,6 +9,6 @@ namespace Xilium.CefGlue.Interop
     
     internal static unsafe partial class libcef
     {
-        public const int CEF_REVISION = 700;
+        public const int CEF_REVISION = 705;
     }
 }
