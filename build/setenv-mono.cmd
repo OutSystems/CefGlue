b08ac3a252fa2066b9e632170e6023ab3f3a86f3
Dmitry Azaraev <dmitry.azaraev@gmail.com>
2013-04-03 23:24:51 +0300

    Fix build script for mono path detection on Windows (x86).

--- build/setenv-mono.cmd
+++ build/setenv-mono.cmd
@@ -1,3 +1,9 @@
 @echo off
+if exist "%ProgramFiles(x86)%\Mono-2.10.8\bin\setmonopath.bat" (
 call "%ProgramFiles(x86)%\Mono-2.10.8\bin\setmonopath"
+) else if exist "%ProgramFiles%\Mono-2.10.8\bin\setmonopath.bat" (
+call "%ProgramFiles%\Mono-2.10.8\bin\setmonopath.bat"
+) else (
+exit /b 1
+)
 exit /b 0
