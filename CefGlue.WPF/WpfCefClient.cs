8924ac7c3b0cbd4a37fd700736afe58a776debac
artem_golubev <none@none>
2013-04-16 18:59:23 -0700

    Fixed cursor not changing issue, Added logging and Switched to a separate WPF dll approach
    --HG--
    branch : cursor-fix-and-wpf

---
+++ CefGlue.WPF/WpfCefClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xilium.CefGlue.Helpers.Log;
+
+namespace Xilium.CefGlue.WPF
+{
+    internal sealed class WpfCefClient : CefClient
+    {
+        private WpfCefBrowser _owner;
+
+        private WpfCefLifeSpanHandler _lifeSpanHandler;
+        private WpfCefDisplayHandler _displayHandler;
+        private WpfCefRenderHandler _renderHandler;
+
+        public WpfCefClient(WpfCefBrowser owner)
+        {
+            if (owner == null) throw new ArgumentNullException("owner");
+
+            _owner = owner;
+
+            _lifeSpanHandler = new WpfCefLifeSpanHandler(owner);
+            _displayHandler = new WpfCefDisplayHandler(owner);
+            _renderHandler = new WpfCefRenderHandler(owner, new NLogLogger("WpfCefRenderHandler"), new UiHelper(new NLogLogger("WpfCefRenderHandler")));
+        }
+
+        protected override CefLifeSpanHandler GetLifeSpanHandler()
+        {
+            return _lifeSpanHandler;
+        }
+
+        protected override CefDisplayHandler GetDisplayHandler()
+        {
+            return _displayHandler;
+        }
+
+        protected override CefRenderHandler GetRenderHandler()
+        {
+            return _renderHandler;
+        }
+    }
+}
