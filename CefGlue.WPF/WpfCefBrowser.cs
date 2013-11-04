49db218ae72efe256a16356b0e349e149f86afcb
Timo Schwierzeck <tschwierzeck@aagon.com>
2013-11-04 15:03:12 +0100

    Implemented ExecuteJavaScript method.

--- CefGlue.WPF/WpfCefBrowser.cs
+++ CefGlue.WPF/WpfCefBrowser.cs
@@ -129,6 +129,12 @@ namespace Xilium.CefGlue.WPF
             this.Content = _browserPageImage;
         }
 
+        public void ExecuteJavaScript(string code, string url, int line)
+        {
+            this._browser.GetMainFrame().ExecuteJavaScript(code, url, line);
+        }
+
+
         protected override Size ArrangeOverride(Size arrangeBounds)
         {
             var size = base.ArrangeOverride(arrangeBounds);
