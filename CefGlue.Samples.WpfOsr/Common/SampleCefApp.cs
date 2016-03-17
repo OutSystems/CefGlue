c22a76d8b1cdc03ba00956c1e80f722ec3b5c25b
Dmitry Azaraev <dmitry.azaraev@gmail.com>
2016-03-18 00:27:19 +0300

    Fix WpfOsr sample by disabling gpu.

--- CefGlue.Samples.WpfOsr/Common/SampleCefApp.cs
+++ CefGlue.Samples.WpfOsr/Common/SampleCefApp.cs
@@ -11,5 +11,16 @@
         public SampleCefApp()
         {
         }
+
+        protected override void OnBeforeCommandLineProcessing(string processType, CefCommandLine commandLine)
+        {
+            if (string.IsNullOrEmpty(processType))
+            {
+                commandLine.AppendSwitch("disable-gpu");
+                commandLine.AppendSwitch("disable-gpu-compositing");
+                commandLine.AppendSwitch("enable-begin-frame-scheduling");
+                commandLine.AppendSwitch("disable-smooth-scrolling");
+            }
+        }
     }
 }
