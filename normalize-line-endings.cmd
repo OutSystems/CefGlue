593367eff51a5e4ce1dfa95c0f1903527ffb4695
Dmitry Azaraev <dmitry.azaraev@gmail.com>
2018-06-01 16:59:08 +0300

    Assuming git configured as core.autocrlf=true, so we no more need care too much about line endings. This tool sometimes still will be useful to normalize headers or missed BOM.

--- normalize-line-endings.cmd
+++ normalize-line-endings.cmd
@@ -1,5 +1,5 @@
 @echo off
-"%~dp0\tools\xifixeol" "%~dp0\" *.cs lf utf8
-"%~dp0\tools\xifixeol" "%~dp0\" *.csproj lf utf8
-"%~dp0\tools\xifixeol" "%~dp0\" *.h lf ascii
-"%~dp0\tools\xifixeol" "%~dp0\" *.py lf ascii
+"%~dp0\tools\xifixeol" "%~dp0\" *.cs crlf utf8
+"%~dp0\tools\xifixeol" "%~dp0\" *.csproj crlf utf8
+"%~dp0\tools\xifixeol" "%~dp0\" *.h crlf ascii
+"%~dp0\tools\xifixeol" "%~dp0\" *.py crlf ascii
