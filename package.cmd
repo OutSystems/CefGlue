898394bba7c9bbcf1e90fd5c3713fc749e20f7d2
alvesmiguel1 <miguel.alves@outsystems.com>
2019-10-04 18:42:49 +0100

    Updated NuGet support for CefGlue projects (net472 only)

--- package.cmd
+++ package.cmd
@@ -1,3 +1,3 @@
-NuGet\nuget pack NuGet\CefGlue.Common.nuspec -Version 1.0.8 -Properties "RedistVersion=75.1.14"
-NuGet\nuget pack NuGet\CefGlue.Avalonia.nuspec -Version 1.0.5
-NuGet\nuget pack NuGet\CefGlue.Wpf.nuspec -Version 1.0.5
+NuGet\nuget pack NuGet\CefGlue.Common.nuspec -Version 75.1.15 -Properties "RedistVersion=75.1.14"
+NuGet\nuget pack NuGet\CefGlue.Avalonia.nuspec -Version 75.1.15
+NuGet\nuget pack NuGet\CefGlue.Wpf.nuspec -Version 75.1.15
