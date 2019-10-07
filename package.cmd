9a32f4734a21dc563f61fe286a1ee3b4e30a0181
João Neves <joao.neves@outsystems.com>
2019-10-07 14:44:33 +0100

    Parameterized nuget script

--- package.cmd
+++ package.cmd
@@ -1,3 +1,3 @@
-NuGet\nuget pack NuGet\CefGlue.Common.nuspec -Version 75.1.15 -Properties "RedistVersion=75.1.14"
-NuGet\nuget pack NuGet\CefGlue.Avalonia.nuspec -Version 75.1.15
-NuGet\nuget pack NuGet\CefGlue.Wpf.nuspec -Version 75.1.15
+NuGet\nuget pack NuGet\CefGlue.Common.nuspec -Version %1 -Properties "RedistVersion=75.1.14"
+NuGet\nuget pack NuGet\CefGlue.Avalonia.nuspec -Version %1
+NuGet\nuget pack NuGet\CefGlue.Wpf.nuspec -Version %1
