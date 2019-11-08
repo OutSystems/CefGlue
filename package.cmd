5a429e1c206612f417c2055ce033b810e9c5d38c
João Neves <joao.neves@outsystems.com>
2019-11-08 19:45:29 +0000

    Fixed build config

--- package.cmd
+++ package.cmd
@@ -1,3 +1,3 @@
 NuGet\nuget pack NuGet\CefGlue.Common.nuspec -Version %1 -Properties "RedistVersion=75.1.14" -OutputDirectory "Nuget\output"
-NuGet\nuget pack NuGet\CefGlue.Avalonia.nuspec -Version %1 -OutputDirectory "Nuget\output"
+rem NuGet\nuget pack NuGet\CefGlue.Avalonia.nuspec -Version %1 -OutputDirectory "Nuget\output"
 NuGet\nuget pack NuGet\CefGlue.Wpf.nuspec -Version %1 -OutputDirectory "Nuget\output"
