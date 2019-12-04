47e04683d9f8baf4ecac40223979d1300f3a1cf3
joaoneves <joaomiguelneves@gmail.com>
2019-12-04 16:27:47 +0000

    Improved samples

--- CefGlue.Demo.WPF/MainWindow.xaml.cs
+++ CefGlue.Demo.WPF/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
+using System.Windows;
+using System.Windows.Controls;
+
 namespace Xilium.CefGlue.Demo.WPF
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Windows;
-    using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -22,20 +11,38 @@ namespace Xilium.CefGlue.Demo.WPF
         public MainWindow()
         {
             InitializeComponent();
+            CreateNewTab();
         }
 
-        protected override void OnClosed(EventArgs e)
-        {
-            browser.Dispose();
-            base.OnClosed(e);
-        }
+        private BrowserView ActiveBrowserView => (BrowserView) tabControl.SelectedContent;
 
-        private void addressTextBox_KeyDown_1(object sender, KeyEventArgs e)
+        private void CreateNewTab()
         {
-            if (e.Key == Key.Return)
+            tabControl.Items.Add(new TabItem()
             {
-                browser.Address = (addressTextBox.Text);
-            }
+                Header = "New Tab",
+                Content = new BrowserView()
+            });
+        }
+
+        private void OnNewTabMenuItemClick(object sender, RoutedEventArgs e)
+        {
+            CreateNewTab();
+        }
+
+        private void OnEvaluateJavascriptMenuItemClick(object sender, RoutedEventArgs e)
+        {
+            ActiveBrowserView.EvaluateJavascript();
+        }
+
+        private void OnBindJavascriptObjectMenuItemClick(object sender, RoutedEventArgs e)
+        {
+            ActiveBrowserView.BindJavascriptObject();
+        }
+
+        private void OnOpenDevToolsMenuItemClick(object sender, RoutedEventArgs e)
+        {
+            ActiveBrowserView.OpenDevTools();
         }
     }
 }
