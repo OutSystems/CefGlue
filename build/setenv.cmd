55d6d169feaab60844d7d9205794ea78d3545910
Dmitry Azaraev <dmitry.azaraev@gmail.com>
2020-07-20 16:04:49 +0300

    Make build.cmd to work (currently VS2019 is required) or run from developer's command prompt.

--- build/setenv.cmd
+++ build/setenv.cmd
@@ -1,18 +1,4 @@
 @echo off
 
-:: Always using x86 toolset.
-goto x86
-
-if "%PROCESSOR_ARCHITECTURE%" == "x86" goto :x86
-if "%PROCESSOR_ARCHITECTURE%" == "AMD64" goto :x64
-
-echo.ERROR^^^! Unknown processor architecture.
-exit /b 1
-
-:x86
-set path=%ProgramFiles(x86)%\MSBuild\14.0\Bin;%ProgramFiles%\MSBuild\14.0\Bin;%WINDIR%\Microsoft.NET\Framework\v4.0.30319;%PATH%
-goto :eof
-
-:x64
-set path=%ProgramFiles(x86)%\MSBuild\14.0\Bin;%WINDIR%\Microsoft.NET\Framework64\v4.0.30319;%PATH%
+set path=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin;%ProgramFiles%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin;%PATH%
 goto :eof
