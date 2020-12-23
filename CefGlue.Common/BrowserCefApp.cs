838aec22d898cef326cfb3d7f4c02ed09262ae89
João Neves <joao.neves@outsystems.com>
2020-12-23 18:29:21 +0000

    Fixed command line args set

--- CefGlue.Common/BrowserCefApp.cs
+++ CefGlue.Common/BrowserCefApp.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Xilium.CefGlue.Common.Handlers;
 using Xilium.CefGlue.Common.InternalHandlers;
 using Xilium.CefGlue.Common.Shared;
@@ -32,11 +32,8 @@ namespace Xilium.CefGlue.Common
                 if (_flags != null)
                 {
                     foreach (var flag in _flags)
-                    {
-                        if (!commandLine.HasSwitch(flag.Key))
-                        {
-                            commandLine.AppendSwitch(flag.Key, flag.Value);
-                        }
+                    {     
+                        commandLine.AppendSwitch(flag.Key, flag.Value);
                     }
                 }
             }
