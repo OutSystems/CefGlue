419c863c89310c661dd3dbeda978cc9028ee3a2f
João Malés <joao.males@outsystems.com>
2021-01-08 10:30:00 +0000

    Corrected cef.redist version.

--- package.cmd
+++ package.cmd
@@ -1,3 +1,3 @@
-NuGet\nuget pack NuGet\CefGlue.Common.nuspec -Version %1 -Properties "RedistVersion=85.3.12" -OutputDirectory "Nuget\output"
+NuGet\nuget pack NuGet\CefGlue.Common.nuspec -Version %1 -Properties "RedistVersion=85.3.13" -OutputDirectory "Nuget\output"
 NuGet\nuget pack NuGet\CefGlue.Wpf.nuspec -Version %1 -OutputDirectory "Nuget\output"
 NuGet\nuget pack NuGet\CefGlue.Avalonia.nuspec -Version %1 -OutputDirectory "Nuget\output"
