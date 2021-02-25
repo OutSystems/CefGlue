9bd3da7357e008ce395a7f2af69eee7935928aa0
Dmitry Azaraev <dmitry.azaraev@gmail.com>
2021-02-25 23:11:12 +0300

    Update to CEF 89.0.5+gc1f90d8+chromium-89.0.4389.40.

--- CefGlue/Interop/version.g.cs
+++ CefGlue/Interop/version.g.cs
@@ -9,15 +9,15 @@ namespace Xilium.CefGlue.Interop
     
     internal static unsafe partial class libcef
     {
-        public const string CEF_VERSION = "88.1.6+g4fe33a1+chromium-88.0.4324.96";
-        public const int CEF_VERSION_MAJOR = 88;
-        public const int CEF_COMMIT_NUMBER = 2291;
-        public const string CEF_COMMIT_HASH = "4fe33a194ad4780baa07d7d9bbac4b6ef8ce7d77";
+        public const string CEF_VERSION = "89.0.5+gc1f90d8+chromium-89.0.4389.40";
+        public const int CEF_VERSION_MAJOR = 89;
+        public const int CEF_COMMIT_NUMBER = 2295;
+        public const string CEF_COMMIT_HASH = "c1f90d8c933dce163b74971707dbd79f00f18219";
         
-        public const int CHROME_VERSION_MAJOR = 88;
+        public const int CHROME_VERSION_MAJOR = 89;
         public const int CHROME_VERSION_MINOR = 0;
-        public const int CHROME_VERSION_BUILD = 4324;
-        public const int CHROME_VERSION_PATCH = 96;
+        public const int CHROME_VERSION_BUILD = 4389;
+        public const int CHROME_VERSION_PATCH = 40;
         
         public const string CEF_API_HASH_UNIVERSAL = "89715b43c948313782d2546131f510eab1975844";
         
