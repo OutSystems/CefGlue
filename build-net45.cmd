ea21322f3423d24491e62d4d50ab18b5678a19e7
Dmitry Azaraev <dmitry.azaraev@gmail.com>
2021-03-07 23:45:13 +0300

    Remove obsolete build scripts.

--- build-net45.cmd
+++
@@ -1,1 +0,0 @@
-@build.cmd /p:Configuration=Release;TargetFrameworkVersion=v4.5;TargetFrameworkProfile= %*
--- build/setenv.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-set path=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin;%ProgramFiles%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin;%PATH%
-goto :eof
