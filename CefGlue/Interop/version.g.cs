864d1fdf2c48e96fb68aa531ed0b8ecd5b3e3098
Dmitry Azaraev <dmitry.azaraev@gmail.com>
2021-03-23 21:02:29 +0300

    Update to CEF 90.0.4+gdc12a89+chromium-90.0.4430.30.

--- CefGlue/Interop/version.g.cs
+++ CefGlue/Interop/version.g.cs
@@ -9,20 +9,20 @@ namespace Xilium.CefGlue.Interop
     
     internal static unsafe partial class libcef
     {
-        public const string CEF_VERSION = "89.0.5+gc1f90d8+chromium-89.0.4389.40";
-        public const int CEF_VERSION_MAJOR = 89;
-        public const int CEF_COMMIT_NUMBER = 2295;
-        public const string CEF_COMMIT_HASH = "c1f90d8c933dce163b74971707dbd79f00f18219";
+        public const string CEF_VERSION = "90.0.4+gdc12a89+chromium-90.0.4430.30";
+        public const int CEF_VERSION_MAJOR = 90;
+        public const int CEF_COMMIT_NUMBER = 2315;
+        public const string CEF_COMMIT_HASH = "dc12a89f28f628cca699acbfe3cb0bf4f9612957";
         
-        public const int CHROME_VERSION_MAJOR = 89;
+        public const int CHROME_VERSION_MAJOR = 90;
         public const int CHROME_VERSION_MINOR = 0;
-        public const int CHROME_VERSION_BUILD = 4389;
-        public const int CHROME_VERSION_PATCH = 40;
+        public const int CHROME_VERSION_BUILD = 4430;
+        public const int CHROME_VERSION_PATCH = 30;
         
-        public const string CEF_API_HASH_UNIVERSAL = "89715b43c948313782d2546131f510eab1975844";
+        public const string CEF_API_HASH_UNIVERSAL = "694bc8d68956bd174529f72fea5710929ac8bbf0";
         
-        public const string CEF_API_HASH_PLATFORM_WIN = "80648a2c5a87db1581fdb994b7154ed77d74a3c5";
-        public const string CEF_API_HASH_PLATFORM_MACOS = "3af393a2bf165edd934c5a59f6e6fce8a4bb579c";
-        public const string CEF_API_HASH_PLATFORM_LINUX = "8049cab9a43c1d554ccdd4dd3d5e38ecebce42af";
+        public const string CEF_API_HASH_PLATFORM_WIN = "597ae4ad020de2665469e7de990d784bfda8d514";
+        public const string CEF_API_HASH_PLATFORM_MACOS = "fa3b4a918791bf53a4a8715ac9f0c843a3c08061";
+        public const string CEF_API_HASH_PLATFORM_LINUX = "83ff6d6fb2e0ac8447f68fa6141091cc9f666ca0";
     }
 }
