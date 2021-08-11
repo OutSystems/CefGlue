9b0bc8795ee0a22135364712d78bafc22419eae2
Miguel Alves <alvesmiguel1@users.noreply.github.com>
2021-08-11 12:22:13 +0100

    Changed CefGlue packages deployment

--- package.cmd
+++
@@ -1,3 +0,0 @@
-dotnet pack CefGlue.Avalonia\Cefglue.Avalonia.csproj -p:Version=%1 -c %2 --no-build -o "Nuget\output"
-dotnet pack CefGlue.Common\Cefglue.Common.csproj -p:Version=%1 -c %2 --no-build -o "Nuget\output"
-NuGet\nuget pack NuGet\CefGlue.Wpf.nuspec -Version %1 -properties Configuration=%2 -OutputDirectory "Nuget\output"
