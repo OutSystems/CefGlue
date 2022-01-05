868ce0bef5ed0987368056db3c3ed178d9cb2cef
João Neves <joao.neves@outsystems.com>
2022-01-05 15:19:46 +0000

    Update DataMarkers.cs

--- CefGlue.Common.Shared/Serialization/DataMarkers.cs
+++ CefGlue.Common.Shared/Serialization/DataMarkers.cs
@@ -2,7 +2,7 @@
 {
     public class DataMarkers
     {
-        // special data markers used for distinguish the several string types
+        // special data markers used to distinguish the several string types
         public const string DateTimeMarker = "#d#";
         public const string StringMarker = "#s#";
         
