d2533d5b5564ec4b5f1f6ea70ba8e8cb3e924233
João Neves <joao.neves@outsystems.com>
2022-01-07 12:57:28 +0000

    Revert using

--- CefGlue.BrowserProcess/ObjectBinding/V8FunctionHandler.cs
+++ CefGlue.BrowserProcess/ObjectBinding/V8FunctionHandler.cs
@@ -18,31 +18,31 @@ namespace Xilium.CefGlue.BrowserProcess.ObjectBinding
 
         protected override bool Execute(string name, CefV8Value obj, CefV8Value[] arguments, out CefV8Value returnValue, out string exception)
         {
-            using (var cefArgs = CefListValue.Create())
+            var cefArgs = CefListValue.Create();
+            // create a copy of the args to pass to the browser process
+            for (var i = 0; i < arguments.Length; i++)
             {
-                // create a copy of the args to pass to the browser process
-                for (var i = 0; i < arguments.Length; i++)
-                {
-                    V8ValueSerialization.SerializeV8ObjectToCefValue(arguments[i], new CefListWrapper(cefArgs, i));
-                }
+                V8ValueSerialization.SerializeV8ObjectToCefValue(arguments[i], new CefListWrapper(cefArgs, i));
+            }
 
-                var message = new Messages.NativeObjectCallRequest()
-                {
-                    ObjectName = _objectName, MemberName = name, ArgumentsIn = cefArgs
-                };
+            var message = new Messages.NativeObjectCallRequest()
+            {
+                ObjectName = _objectName, 
+                MemberName = name, 
+                ArgumentsIn = cefArgs
+            };
 
-                var promiseHolder = _functionCallHandler(message);
+            var promiseHolder = _functionCallHandler(message);
 
-                if (promiseHolder != null)
-                {
-                    returnValue = promiseHolder.Promise;
-                    exception = null;
-                }
-                else
-                {
-                    returnValue = null;
-                    exception = "Failed to create promise";
-                }
+            if (promiseHolder != null)
+            {
+                returnValue = promiseHolder.Promise;
+                exception = null;
+            }
+            else
+            {
+                returnValue = null;
+                exception = "Failed to create promise";
             }
 
             return true;
