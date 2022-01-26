b6e8c011860de5eaa68dc362d7ce04446616a1d0
Luiz Alberto de Castro Santos <luiz.santos@outsystems.com>
2022-01-26 17:36:48 +0000

    RDOIAT-374: Improve serialization performance (#40)
    * Improve serialization performance
    
    * Fixed build error
    
    * Fixed tests
    
    * Expose Json package
    
    * Fix deserialization of empty strings
    
    * Update CefGlue.BrowserProcess/Serialization/V8ValueSerialization.cs
    
    Co-authored-by: João Neves <joao.neves@outsystems.com>
    
    * Test empty string parameter
    
    * Fix failing test
    
    * Add null string param test
    
    * Fix failing test
    
    Co-authored-by: João Neves <joao.neves@outsystems.com>
    Co-authored-by: brunocunhasilva <bruno.cunhasilva@outsystems.com>
    Co-authored-by: Bruno Cunha e Silva <brunocunhasilva@users.noreply.github.com>

--- CefGlue.Tests/Javascript/NativeObjectInteropTests.cs
+++ CefGlue.Tests/Javascript/NativeObjectInteropTests.cs
@@ -9,6 +9,8 @@ namespace CefGlue.Tests.Javascript
 {
     public class NativeObjectInteropTests : TestBase
     {
+        const string Date = "1995-12-17T03:24:00Z";
+
         protected const string ObjName = "nativeObj";
 
         protected NativeObject nativeObject;
@@ -17,10 +19,16 @@ namespace CefGlue.Tests.Javascript
         {
             public string Name = null;
             public int Age = 0;
+            public DateTime BirthDate = default;
+            public byte[] Photo = default;
         }
 
         protected class NativeObject
         {
+            private readonly TaskCompletionSource<object> _tcs = new TaskCompletionSource<object>();
+
+            public Task<object> ResultTask => _tcs.Task;
+                
             public event Action TestCalled;
 
             public void Test()
@@ -28,11 +36,28 @@ namespace CefGlue.Tests.Javascript
                 TestCalled?.Invoke();
             }
 
+            public void SetResult(object result)
+            {
+                _tcs.SetResult(result);
+            }
+
+            public void SetPersonResult(Person result)
+            {
+                _tcs.SetResult(result);
+            }
+
             public event Action<object[]> MethodWithParamsCalled;
 
-            public void MethodWithParams(string param1, int param2)
+            public void MethodWithParams(string param1, int param2, DateTime param3, bool param4)
             {
-                MethodWithParamsCalled?.Invoke(new object[] { param1, param2 });
+                MethodWithParamsCalled?.Invoke(new object[] { param1, param2, param3, param4 });
+            }
+            
+            public event Action<object[]> MethodWithStringParamCalled;
+
+            public void MethodWithStringParam(string param1)
+            {
+                MethodWithStringParamCalled?.Invoke(new object[] { param1 });
             }
 
             public event Action<object[]> MethodWithObjectParamCalled;
@@ -40,6 +65,11 @@ namespace CefGlue.Tests.Javascript
             public void MethodWithObjectParam(Person param)
             {
                 MethodWithObjectParamCalled?.Invoke(new object[] { param });
+            }
+
+            public object MethodWithNullReturn()
+            {
+                return null;
             }
 
             public event Func<int, Task<string>> AsyncMethodCalled;
@@ -62,9 +92,19 @@ namespace CefGlue.Tests.Javascript
                 return Task.FromException(new Exception("error"));
             }
             
-            public string MethodWithReturn()
+            public string MethodWithStringReturn()
             {
                 return "this is the result";
+            }
+
+            public DateTime MethodWithDateTimeReturn()
+            {
+                return DateTime.Parse(Date);
+            }
+
+            public Person MethodWithObjectReturn()
+            {
+                return new Person() {Name = "John", Age = 30, BirthDate = DateTime.Parse(Date)};
             }
         }
 
@@ -115,17 +155,49 @@ namespace CefGlue.Tests.Javascript
         {
             const string Arg1 = "test";
             const int Arg2 = 5;
-
+            
             var taskCompletionSource = new TaskCompletionSource<object[]>();
             nativeObject.MethodWithParamsCalled += (args) => taskCompletionSource.SetResult(args);
 
-            Execute($"{ObjName}.methodWithParams('{Arg1}', {Arg2})");
-            
+            Execute($"{ObjName}.methodWithParams('{Arg1}', {Arg2}, new Date('{Date}'), true)");
+
             var result = await taskCompletionSource.Task;
 
-            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(4, result.Length);
             Assert.AreEqual(Arg1, result[0]);
             Assert.AreEqual(Arg2, result[1]);
+            Assert.AreEqual(DateTime.Parse(Date), result[2]);
+            Assert.AreEqual(true, result[3]);
+        }
+        
+        [Test]
+        public async Task MethodEmptyStringParamIsPassed()
+        {
+            const string Arg1 = "";
+            
+            var taskCompletionSource = new TaskCompletionSource<object[]>();
+            nativeObject.MethodWithStringParamCalled += (args) => taskCompletionSource.SetResult(args);
+
+            Execute($"{ObjName}.methodWithStringParam('{Arg1}')");
+
+            var result = await taskCompletionSource.Task;
+
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(Arg1, result[0]);
+        }
+        
+        [Test]
+        public async Task MethodNullStringParamIsPassed()
+        {
+            var taskCompletionSource = new TaskCompletionSource<object[]>();
+            nativeObject.MethodWithStringParamCalled += (args) => taskCompletionSource.SetResult(args);
+
+            Execute($"{ObjName}.methodWithStringParam(null)");
+
+            var result = await taskCompletionSource.Task;
+
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(null, result[0]);
         }
 
         [Test]
@@ -134,7 +206,7 @@ namespace CefGlue.Tests.Javascript
             var taskCompletionSource = new TaskCompletionSource<object[]>();
             nativeObject.MethodWithObjectParamCalled += (args) => taskCompletionSource.SetResult(args);
 
-            Execute($"{ObjName}.methodWithObjectParam({{'Name': 'cef', 'Age': 10 }})");
+            Execute($"{ObjName}.methodWithObjectParam({{'Name': 'cef', 'Age': 10, 'BirthDate': new Date('{Date}') }})");
 
             var result = await taskCompletionSource.Task;
             Assert.AreEqual(1, result.Length);
@@ -143,19 +215,51 @@ namespace CefGlue.Tests.Javascript
             var arg = (Person) result[0];
             Assert.AreEqual("cef", arg.Name);
             Assert.AreEqual(10, arg.Age);
+            Assert.AreEqual(DateTime.Parse(Date), arg.BirthDate);
         }
 
         [Test]
-        public async Task NativeObjectMethodResultIsReturned()
+        public async Task NativeObjectMethodNullResultIsReturned()
         {
-            var taskCompletionSource = new TaskCompletionSource<object[]>();
-            nativeObject.MethodWithParamsCalled += (args) => taskCompletionSource.SetResult(args);
-            
-            Execute($"{ObjName}.methodWithReturn().then(r => {ObjName}.methodWithParams(r, 0));");
+            Execute($"{ObjName}.methodWithNullReturn().then(r => {ObjName}.setResult(r));");
 
-            var result = await taskCompletionSource.Task;
+            var result = await nativeObject.ResultTask;
 
-            Assert.AreEqual(nativeObject.MethodWithReturn(), result[0]);
+            Assert.AreEqual(nativeObject.MethodWithNullReturn(), result);
+        }
+
+        [Test]
+        public async Task NativeObjectMethodStringResultIsReturned()
+        {
+            Execute($"{ObjName}.methodWithStringReturn().then(r => {ObjName}.setResult(r));");
+
+            var result = await nativeObject.ResultTask;
+
+            Assert.AreEqual(nativeObject.MethodWithStringReturn(), result);
+        }
+
+        [Test]
+        public async Task NativeObjectMethodDateTimeResultIsReturned()
+        {
+            Execute($"{ObjName}.methodWithDateTimeReturn().then(r => {ObjName}.setResult(r));");
+
+            var result = await nativeObject.ResultTask;
+
+            Assert.AreEqual(nativeObject.MethodWithDateTimeReturn(), result);
+        }
+
+        [Test]
+        public async Task NativeObjectMethodObjectResultIsReturned()
+        {
+            Execute($"{ObjName}.methodWithObjectReturn().then(r => {ObjName}.setPersonResult(r));");
+
+            var result = (Person) await nativeObject.ResultTask;
+
+            var expected = nativeObject.MethodWithObjectReturn();
+            Assert.AreEqual(expected.Name, result.Name);
+            Assert.AreEqual(expected.Age, result.Age);
+            Assert.AreEqual(expected.BirthDate, result.BirthDate);
+            Assert.AreEqual(expected.Photo, result.Photo);
         }
         
         [Test]
@@ -198,41 +302,32 @@ namespace CefGlue.Tests.Javascript
         [Test]
         public async Task AsyncMethodResultIsReturned()
         {
-            var taskCompletionSource = new TaskCompletionSource<object[]>();
-            nativeObject.MethodWithParamsCalled += (args) => taskCompletionSource.SetResult(args);
+            Execute($"{ObjName}.asyncMethod(0).then(r => {ObjName}.setResult(r));");
 
-            Execute($"{ObjName}.asyncMethod(0).then(r => {ObjName}.methodWithParams(r, 0));");
+            var result = await nativeObject.ResultTask; ;
 
-            var result = await taskCompletionSource.Task;
-
-            Assert.AreEqual(nativeObject.AsyncMethod(0).Result, result[0]);
+            Assert.AreEqual(nativeObject.AsyncMethod(0).Result, result);
         }
 
         [Test]
         public async Task MethodExceptionIsReturned()
         {
-            var taskCompletionSource = new TaskCompletionSource<object[]>();
-            nativeObject.MethodWithParamsCalled += (args) => taskCompletionSource.SetResult(args);
+            Execute($"{ObjName}.methodReturnException().catch(r => {ObjName}.setResult(r));");
 
-            Execute($"{ObjName}.methodReturnException().catch(r => {ObjName}.methodWithParams(r, 0));");
-
-            var result = await taskCompletionSource.Task;
+            var result = await nativeObject.ResultTask;
 
             var msg = Assert.Throws<Exception>(() => nativeObject.MethodReturnException()).Message;
-            Assert.AreEqual(msg, result[0]);
+            Assert.AreEqual(msg, result);
         }
 
         [Test]
         public async Task AsyncMethodExceptionIsReturned()
         {
-            var taskCompletionSource = new TaskCompletionSource<object[]>();
-            nativeObject.MethodWithParamsCalled += (args) => taskCompletionSource.SetResult(args);
+            Execute($"{ObjName}.asyncMethodReturnException().catch(r => {ObjName}.setResult(r));");
 
-            Execute($"{ObjName}.asyncMethodReturnException().catch(r => {ObjName}.methodWithParams(r, 0));");
+            var result = await nativeObject.ResultTask;
 
-            var result = await taskCompletionSource.Task;
-
-            Assert.AreEqual(nativeObject.AsyncMethodReturnException().Exception.Message, result[0]);
+            Assert.AreEqual(nativeObject.AsyncMethodReturnException().Exception.Message, result);
         }
     }
 }
