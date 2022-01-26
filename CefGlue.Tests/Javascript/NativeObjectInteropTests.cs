4f02479f8bfb830885fe1048f505aa3db188ab1b
brunocunhasilva <bruno.cunhasilva@outsystems.com>
2022-01-26 17:06:25 +0000

    Add null string param test

--- CefGlue.Tests/Javascript/NativeObjectInteropTests.cs
+++ CefGlue.Tests/Javascript/NativeObjectInteropTests.cs
@@ -185,6 +185,22 @@ namespace CefGlue.Tests.Javascript
             Assert.AreEqual(1, result.Length);
             Assert.AreEqual(Arg1, result[0]);
         }
+        
+        [Test]
+        public async Task MethodNullStringParamIsPassed()
+        {
+            const string Arg1 = null;
+            
+            var taskCompletionSource = new TaskCompletionSource<object[]>();
+            nativeObject.MethodWithStringParamCalled += (args) => taskCompletionSource.SetResult(args);
+
+            Execute($"{ObjName}.methodWithStringParam({Arg1})");
+
+            var result = await taskCompletionSource.Task;
+
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(Arg1, result[0]);
+        }
 
         [Test]
         public async Task MethodWithObjectParamIsPassed()
