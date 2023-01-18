179b872b3085b371017b3bc9114127673b8ce651
Luiz Santos <luiz.santos@outsystems.com>
2023-01-18 16:42:16 +0000

    Fix generator script

--- CefGlue.Interop.Gen/gen-cef3.sh
+++ CefGlue.Interop.Gen/gen-cef3.sh
@@ -3,5 +3,5 @@
 # @echo off
 # Determine python path by inspecting registry: HKCU/HKLM SOFTWARE\Python\PythonCore\${PYTHON_VERSION}\InstallPath
 # And then try some default locations: c:\python27amd64 , c:\python27
-/usr/bin/python3 -B cefglue_interop_gen.py --cpp-header-dir include --cefglue-dir ..\CefGlue\ --no-backup
+/usr/bin/python3 -B cefglue_interop_gen.py --cpp-header-dir include --cefglue-dir ../CefGlue/ --no-backup
 read -p "Press any key to resume ..."
