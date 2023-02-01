33037fa82762bbd167971d630d86e4c63b5711dc
Luiz Alberto de Castro Santos <luiz.santos@outsystems.com>
2023-02-01 11:35:52 +0000

    CEF 5249: Fix Javascript context events not being fired (#63)
    * Fix js context events not being fired
    
    * Remove comment
    
    * Remove unused messages
    
    * Remove debug logs
    
    * Remove unused files
    
    * Extract javascript events from javascript execution engine
    
    * Apply review suggestions

--- CefGlue.Common/CommonBrowserAdapter.cs
+++ CefGlue.Common/CommonBrowserAdapter.cs
@@ -155,7 +155,7 @@ namespace Xilium.CefGlue.Common
             set => BrowserHost?.SetZoomLevel(value);
         }
 
-        public bool IsJavascriptEngineInitialized => _javascriptExecutionEngine?.IsMainFrameContextInitialized == true;
+        public bool IsJavascriptEngineInitialized { get; private set; }
 
         public CefBrowserSettings Settings { get; } = new CefBrowserSettings();
 
@@ -322,16 +322,6 @@ namespace Xilium.CefGlue.Common
             windowInfo.SetAsChild(hostViewHandle, new CefRectangle(0, 0, width, height));
         }
 
-        private void OnJavascriptExecutionEngineContextCreated(CefFrame frame)
-        {
-            JavascriptContextCreated?.Invoke(_eventsEmitter, new JavascriptContextLifetimeEventArgs(frame));
-        }
-
-        private void OnJavascriptExecutionEngineContextReleased(CefFrame frame)
-        {
-            JavascriptContextReleased?.Invoke(_eventsEmitter, new JavascriptContextLifetimeEventArgs(frame));
-        }
-
         private void OnJavascriptExecutionEngineUncaughtException(JavascriptUncaughtExceptionEventArgs args)
         {
             JavascriptUncaughtException?.Invoke(_eventsEmitter, args);
@@ -417,8 +407,6 @@ namespace Xilium.CefGlue.Common
                 if (dispatcher != null)
                 {
                     var javascriptExecutionEngine = new JavascriptExecutionEngine(dispatcher);
-                    javascriptExecutionEngine.ContextCreated += OnJavascriptExecutionEngineContextCreated;
-                    javascriptExecutionEngine.ContextReleased += OnJavascriptExecutionEngineContextReleased;
                     javascriptExecutionEngine.UncaughtException += OnJavascriptExecutionEngineUncaughtException;
                     _javascriptExecutionEngine = javascriptExecutionEngine;
 
@@ -572,6 +560,24 @@ namespace Xilium.CefGlue.Common
         void ICefBrowserHost.HandleLoadingStateChange(CefBrowser browser, bool isLoading, bool canGoBack, bool canGoForward)
         {
             LoadingStateChange?.Invoke(_eventsEmitter, new LoadingStateChangeEventArgs(isLoading, canGoBack, canGoForward));
+        }
+
+        void ICefBrowserHost.HandleFrameAttached(CefBrowser browser, CefFrame frame, bool reattached)
+        {
+            if (frame.IsMain)
+            {
+                IsJavascriptEngineInitialized = true;
+            }
+            JavascriptContextCreated?.Invoke(_eventsEmitter, new JavascriptContextLifetimeEventArgs(frame));
+        }
+
+        void ICefBrowserHost.HandleFrameDetached(CefBrowser browser, CefFrame frame)
+        {
+            if (frame.IsMain)
+            {
+                IsJavascriptEngineInitialized = false;
+            }
+            JavascriptContextReleased?.Invoke(_eventsEmitter, new JavascriptContextLifetimeEventArgs(frame));
         }
 
         void ICefBrowserHost.HandleOpenContextMenu(CefContextMenuParams parameters, CefMenuModel model, CefRunContextMenuCallback callback)
---
+++ CefGlue.Common/InternalHandlers/CommonCefFrameHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xilium.CefGlue.Common.InternalHandlers
+{
+    internal class CommonCefFrameHandler : CefFrameHandler
+    {
+        private readonly ICefBrowserHost _owner;
+
+        public CommonCefFrameHandler(ICefBrowserHost owner)
+        {
+            _owner = owner;
+        }
+
+        protected override void OnFrameAttached(CefBrowser browser, CefFrame frame, bool reattached)
+        {
+            base.OnFrameAttached(browser, frame, reattached);
+            _owner.HandleFrameAttached(browser, frame, reattached);
+        }
+
+        protected override void OnFrameDetached(CefBrowser browser, CefFrame frame)
+        {
+            base.OnFrameDetached(browser, frame);
+            _owner.HandleFrameDetached(browser, frame);
+        }
+    }
+}
