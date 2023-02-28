5c6f7ebe7f9a2c0f61c75696f6d1ac61413822b3
Bruno Cunha e Silva <brunocunhasilva@users.noreply.github.com>
2023-02-28 15:15:28 +0000

    Preserve references in deserialization 5249 (#73)
    * Preserve references in serialization to allow deeper structures to be serialized
    
    * Clean unwanted changes
    
    * Fix NativeObject DateTime interop tests
    
    * CR improvements
    
    * Remove duplication in Directory.Build.props
    
    * Simplify deserializer state logic. Improve resources usage and performance
    
    * Add missing RootObjectDeserializerState file
    
    * Remove unwanted whitespaces on Directory.Build.props
    
    * Refactor DerserializerState classes and simplify Deserializer logic
    
    * CR improvements.
    Drop DynamicDeserializerState.
    
    * Optimize TypeInfoCache, not loading Properties and Fields for collections and primitive types
    
    * CR improvements
    
    * Fix EvaluateScript for try catch blocks - "Uncaught SyntaxError: Unexpected token 'try'"
    
    * Fix SampleApp EvaluateJavascript that were missing the return keyword
    
    * Fix deserialization of null as structures
    
    * CR improvement

---
+++ CefGlue.Common.Shared/Serialization/State/CollectionDeserializerState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Xilium.CefGlue.Common.Shared.Serialization.State
+{
+    internal class CollectionDeserializerState : IDeserializerState<object>
+    {
+        private readonly JsonTypeInfo _collectionTypeInfo;
+        private readonly JsonTypeInfo _collectionElementTypeInfo;
+        
+        private string _propertyName;
+
+        public CollectionDeserializerState(JsonTypeInfo collectionTypeInfo)
+        {
+            if (collectionTypeInfo.CollectionAddMethod == null)
+            {
+                throw new ArgumentException("Argument must contain an Add method.", nameof(collectionTypeInfo));
+            }
+
+            Value = Activator.CreateInstance(collectionTypeInfo.ObjectType, nonPublic: true);
+            _collectionTypeInfo = collectionTypeInfo;
+            _collectionElementTypeInfo = collectionTypeInfo.EnumerableElementTypeInfo;
+        }
+
+        public object Value { get; }
+
+        public void SetCurrentPropertyName(string value) => _propertyName = value;
+        
+        public JsonTypeInfo CurrentElementTypeInfo => _collectionElementTypeInfo;
+
+        public void SetCurrentElementValue(object value)
+        {
+            var parameters = string.IsNullOrEmpty(_propertyName) ?
+                new[] { value } :
+                new[] { _propertyName, value };
+            _collectionTypeInfo.CollectionAddMethod.Invoke(Value, parameters);
+        }
+    }
+}
---
+++ CefGlue.Common.Shared/Serialization/State/JsonTypeInfoCache.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Xilium.CefGlue.Common.Shared.Serialization.State
+{
+    internal static class JsonTypeInfoCache
+    {
+        private static readonly ConcurrentDictionary<Type, JsonTypeInfo> TypesInfoCache = new ConcurrentDictionary<Type, JsonTypeInfo>();
+
+        public static readonly JsonTypeInfo ObjectTypeInfo = new JsonTypeInfo(typeof(object), TypeCode.Object);
+        public static readonly JsonTypeInfo ObjectDictionaryTypeInfo = new JsonTypeInfo(typeof(Dictionary<string, object>), TypeCode.Object);
+        public static readonly JsonTypeInfo ObjectArrayTypeInfo = new JsonTypeInfo(typeof(object[]), TypeCode.Object);
+
+        public static JsonTypeInfo GetOrAddTypeInfo(Type type)
+        {
+            var typeCode = Type.GetTypeCode(type);
+            switch (typeCode)
+            {
+                case TypeCode.Byte:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.SByte:
+                case TypeCode.Single:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Char:
+                case TypeCode.Boolean:
+                case TypeCode.DateTime:
+                case TypeCode.String:
+                    return new JsonTypeInfo(type, typeCode);
+
+                case TypeCode.DBNull:
+                case TypeCode.Empty:
+                    return ObjectTypeInfo;
+                default:
+                    return 
+                        type == null ? 
+                        null :
+                        TypesInfoCache.GetOrAdd(type, _ => new JsonTypeInfo(type, typeCode));
+            }
+        }
+    }
+}
