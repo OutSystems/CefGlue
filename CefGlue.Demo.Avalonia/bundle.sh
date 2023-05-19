f23f2597470841fcdbb7df86b4d4149aad9f1358
Miguel Alves <alvesmiguel1@users.noreply.github.com>
2023-05-19 08:18:54 +0100

    Update bundle.sh

--- CefGlue.Demo.Avalonia/bundle.sh
+++ CefGlue.Demo.Avalonia/bundle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=osx-x64
+dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=osx-arm64
 
-TARGETAPP=bin/Debug/net6.0/osx-x64/publish/CefGlueDemoAvalonia.app/Contents/MacOS
+TARGETAPP=bin/Debug/ARM64/net6.0/osx-arm64/publish/CefGlueDemoAvalonia.app/Contents/MacOS
 chmod +x "$TARGETAPP/CefGlueBrowserProcess/Xilium.CefGlue.BrowserProcess"
 chmod +x "$TARGETAPP/Xilium.CefGlue.Demo.Avalonia"
