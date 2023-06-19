f0979b0f46877a953de7a09c2b4118546e1ee78f
Miguel Alves <miguel.alves@outsystems.com>
2023-06-19 10:38:05 +0100

    Updated props and targets

---
+++ CefGlue.Demo.Avalonia/bundle-x64.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=osx-x64
+
+TARGETAPP=bin/Debug/AnyCPU/net6.0/osx-x64/publish/CefGlueDemoAvalonia.app/Contents/MacOS
+chmod +x "$TARGETAPP/CefGlueBrowserProcess/Xilium.CefGlue.BrowserProcess"
+chmod +x "$TARGETAPP/Xilium.CefGlue.Demo.Avalonia"
