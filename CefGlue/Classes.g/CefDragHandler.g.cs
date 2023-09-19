79c214448e05a08013a03b9707be9e00a8d3d117
Filipe Campos <filipe.campos@outsystems.com>
2023-09-19 14:06:40 +0100

    RDOIBT-1838 - CefGlue: Duplicate key reference
    Revert replacement of lock by Interlocked

--- CefGlue/Classes.g/CefDragHandler.g.cs
+++ CefGlue/Classes.g/CefDragHandler.g.cs
@@ -18,6 +18,8 @@ namespace Xilium.CefGlue
         private int _refct;
         private cef_drag_handler_t* _self;
         
+        protected object SyncRoot { get { return this; } }
+        
         private cef_drag_handler_t.add_ref_delegate _ds0;
         private cef_drag_handler_t.release_delegate _ds1;
         private cef_drag_handler_t.has_one_ref_delegate _ds2;
@@ -59,30 +61,38 @@ namespace Xilium.CefGlue
         
         private void add_ref(cef_drag_handler_t* self)
         {
-            if (Interlocked.Increment(ref _refct) == 1)
+            lock (SyncRoot)
             {
-                lock (_roots) { _roots.Add((IntPtr)_self, this); }
+                var result = ++_refct;
+                if (result == 1)
+                {
+                    lock (_roots) { _roots.Add((IntPtr)_self, this); }
+                }
             }
         }
         
         private int release(cef_drag_handler_t* self)
         {
-            if (Interlocked.Decrement(ref _refct) == 0)
+            lock (SyncRoot)
             {
-                lock (_roots) { _roots.Remove((IntPtr)_self); }
-                return 1;
+                var result = --_refct;
+                if (result == 0)
+                {
+                    lock (_roots) { _roots.Remove((IntPtr)_self); }
+                    return 1;
+                }
+                return 0;
             }
-            return 0;
         }
         
         private int has_one_ref(cef_drag_handler_t* self)
         {
-            return _refct == 1 ? 1 : 0;
+            lock (SyncRoot) { return _refct == 1 ? 1 : 0; }
         }
         
         private int has_at_least_one_ref(cef_drag_handler_t* self)
         {
-            return _refct != 0 ? 1 : 0;
+            lock (SyncRoot) { return _refct != 0 ? 1 : 0; }
         }
         
         internal cef_drag_handler_t* ToNative()
