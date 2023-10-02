79661bb2b31a6d1ddd83f8424f0d4d7168356d57
José Santos <jose.dos.santos@outsystems.com>
2023-10-02 15:50:02 +0100

    generating files

--- CefGlue/Interop/version.g.cs
+++ CefGlue/Interop/version.g.cs
@@ -6,21 +6,21 @@ namespace Xilium.CefGlue.Interop
     using System;
     using System.Runtime.InteropServices;
     using System.Diagnostics.CodeAnalysis;
-
+    
     internal static unsafe partial class libcef
     {
         public const string CEF_VERSION = "116.0.27+gd8c85ac+chromium-116.0.5845.190";
         public const int CEF_VERSION_MAJOR = 116;
         public const int CEF_COMMIT_NUMBER = 2831;
         public const string CEF_COMMIT_HASH = "d8c85aca1de77ab7e44ce121847380df7499daaa";
-
+        
         public const int CHROME_VERSION_MAJOR = 116;
         public const int CHROME_VERSION_MINOR = 0;
         public const int CHROME_VERSION_BUILD = 5845;
         public const int CHROME_VERSION_PATCH = 190;
-
+        
         public const string CEF_API_HASH_UNIVERSAL = "515045085896c9e45c1aaf734ee201efad822f6a";
-
+        
         public const string CEF_API_HASH_PLATFORM_WIN = "1091d9b7f17abbbf86cbbf50db7106a2bad98ab5";
         public const string CEF_API_HASH_PLATFORM_MACOS = "bfb5a9cb4551e80a0606531399e3244eac457a33";
         public const string CEF_API_HASH_PLATFORM_LINUX = "1ef1d39d8c95b1d6c93fa60a29e158a96f76ab9d";
