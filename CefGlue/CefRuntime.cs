282bb5c3a27487f87d6a2169e73d6e2d2d58a1d1
João Neves <joao.neves@outsystems.com>
2024-07-30 17:49:47 +0100

    Fix build and update packages

--- CefGlue/CefRuntime.cs
+++ CefGlue/CefRuntime.cs
@@ -1,12 +1,10 @@
-﻿using System.IO;
-
-namespace Xilium.CefGlue
+﻿namespace Xilium.CefGlue
 {
     using System;
-    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
     using System.Globalization;
     using System.Runtime.InteropServices;
-    using System.Text;
     using Xilium.CefGlue.Interop;
 
     public static unsafe class CefRuntime
