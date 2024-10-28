502e5bfb692306ba79478fd49d95556fe0be68ce
João Neves <joao.neves@outsystems.com>
2024-10-28 15:42:01 +0000

    Fix wpf demo

--- CefGlue.Demo.WPF/Program.cs
+++ CefGlue.Demo.WPF/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xilium.CefGlue.Common;
 
 namespace Xilium.CefGlue.Demo.WPF
@@ -8,9 +9,16 @@ namespace Xilium.CefGlue.Demo.WPF
         [STAThread]
         private static int Main(string[] args)
         {
+            // generate a unique cache path to avoid problems when launching more than one process
+            // https://www.magpcss.org/ceforum/viewtopic.php?f=6&t=19665
+            var cachePath = Path.Combine(Path.GetTempPath(), "CefGlue_" + Guid.NewGuid().ToString().Replace("-", null));
+            
+            AppDomain.CurrentDomain.ProcessExit += delegate { Cleanup(cachePath); };
+            
             var settings = new CefSettings()
             {
 #if WINDOWLESS
+                // its recommended to leave this off (false), since its less performant and can cause more issues
                 WindowlessRenderingEnabled = true
 #else
                 WindowlessRenderingEnabled = false
@@ -23,6 +31,22 @@ namespace Xilium.CefGlue.Demo.WPF
             app.Run();
 
             return 0;
+        }
+        
+        private static void Cleanup(string cachePath)
+        {
+            CefRuntime.Shutdown(); // must shutdown cef to free cache files (so that cleanup is able to delete files)
+
+            try {
+                var dirInfo = new DirectoryInfo(cachePath);
+                if (dirInfo.Exists) {
+                    dirInfo.Delete(true);
+                }
+            } catch (UnauthorizedAccessException) {
+                // ignore
+            } catch (IOException) {
+                // ignore
+            }
         }
     }
 }
