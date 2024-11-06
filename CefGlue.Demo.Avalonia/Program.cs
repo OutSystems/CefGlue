5aa4ee3fea8cc328e69a054dc30cfe792410efc7
Paolo Iommarini <paolo.iommarini@xenialab.it>
2024-11-06 14:03:26 +0100

    Disable FirstPartySets (YouTube crash)

--- CefGlue.Demo.Avalonia/Program.cs
+++ CefGlue.Demo.Avalonia/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Avalonia;
 using Xilium.CefGlue.Common;
@@ -14,15 +15,15 @@ namespace Xilium.CefGlue.Demo.Avalonia
             // generate a unique cache path to avoid problems when launching more than one process
             // https://www.magpcss.org/ceforum/viewtopic.php?f=6&t=19665
             var cachePath = Path.Combine(Path.GetTempPath(), "CefGlue_" + Guid.NewGuid().ToString().Replace("-", null));
-            
+
             AppDomain.CurrentDomain.ProcessExit += delegate { Cleanup(cachePath); };
-            
+
             AppBuilder.Configure<App>()
                       .UsePlatformDetect()
                       .With(new Win32PlatformOptions())
                       .AfterSetup(_ => CefRuntimeLoader.Initialize(new CefSettings() {
                           RootCachePath = cachePath,
-#if WINDOWLESS 
+#if WINDOWLESS
                           // its recommended to leave this off (false), since its less performant and can cause more issues
                           WindowlessRenderingEnabled = true
 #else
@@ -35,9 +36,13 @@ namespace Xilium.CefGlue.Demo.Avalonia
                             SchemeName = "test",
                             SchemeHandlerFactory = new CustomSchemeHandler()
                         }
-                      }))
+                      },
+                      flags: [
+                          // https://github.com/chromiumembedded/cef/issues/3643
+                          new KeyValuePair<string, string>("disable-features", "FirstPartySets")
+                      ]))
                       .StartWithClassicDesktopLifetime(args);
-                      
+
             return 0;
         }
 
